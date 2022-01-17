fcdfc55856ceb7e9f4860dbc712d4d3b1be8aa73
aleksandrVorobey <69941253+aleksandrVorobey@users.noreply.github.com>
2022-01-17 14:31:53 +0300

    start project

---
+++ weatherAppTest/weatherAppTest/Views/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  weatherAppTest
+//
+//  Created by Александр Воробей on 17.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
